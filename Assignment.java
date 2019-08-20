16f8d0d58f307cbff7cfea8da178c27883241a72
abriley14 <abriley14@gmail.com>
2019-08-20 14:15:03 +0000

    Copy over filees from Monday

---
+++ Assignment.java
@@ -0,0 +1,9 @@
+public class Assignment
+{
+    public static void main(String[] args)
+    {
+        String name = "Andrew";
+        System.out.println("How are you" + ", " + name +
+        "?");
+    }
+}
