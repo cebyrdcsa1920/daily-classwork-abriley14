e079ccaacf280e3a097de09afe8e90e45422c836
abriley14 <abriley14@gmail.com>
2019-09-11 13:40:11 +0000

    Create mthod to add gas to Car

--- week4/Car.java
+++ week4/Car.java
@@ -36,6 +36,16 @@ public class Car
         return amountOfGas;
     }
 
+    /**
+     * Fills up the gas tank
+     *
+     * @param amount gas to add to tank
+     */
+    public void fillUpGasTank(double amount)
+    {
+        amountOfGas += amount;
+    }
+
     public String toString()
     {
         return "This is my Mustang";
