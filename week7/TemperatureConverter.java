14dfdc8fbe3b62bf3e6223db56cb7bf4290db227
abriley14 <abriley14@gmail.com>
2019-09-25 14:26:32 +0000

    updates

--- week7/TemperatureConverter.java
+++ week7/TemperatureConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TemperatureConverter
 {
     private double temperature;
@@ -6,7 +8,7 @@ public class TemperatureConverter
     public TemperatureConverter(double celsius)
     {
         temperature = celsius;
-        isCelsius - true;
+        isCelsius = true;
     }
 
     public void convertToF()
@@ -14,6 +16,16 @@ public class TemperatureConverter
         if (isCelsius)
         {
             temperature = temperature * 9/5 + 32;
+            isCelsius = false;
+        }
+    }
+
+    public void convertToC()
+    {
+        if (!isCelsius)
+        {
+            temperature = (temperature - 32) * 5 / 9;
+            isCelsius = true;
         }
     }
 
@@ -26,9 +38,31 @@ public class TemperatureConverter
         }
         else
         {
-            result += " Fahrenhsit";
+            result += " Fahrenheit";
         }
         return result;
+    }
+
+    public static void main(String[] args)
+    {
+        /* TemperatureConverter myTemp = new TemperatureConverter(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);*/
+
+        Scanner kboard = new Scanner(System.in);
+        System.out.println("Enter a temperature in Celsius: ");
+        double input = kboard.nextDouble();
+        kboard.nextLine();
+        TemperatureConverter myTemp = new TemperatureConverter(input);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
+
     }
 
 }
